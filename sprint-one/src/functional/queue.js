8affe97dd20a5679a670fd0d81e5e871f6b22e6c
Shailee Patel <shaileeee@gmail.com>
2020-10-07 16:29:25 -0700

    Complete Stack using Functional instantiation and Queue with one failing test

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,43 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var count = 0;
+    for (var key in storage) {
+      count++;
+    }
+    if (count !== 0) {
+      storage[count] = value;
+    } else {
+      storage[count] = value;
+      return 1;
+    }
+    return count + 1;
   };
 
   someInstance.dequeue = function() {
+    var count = 0;
+    for (var key in storage) {
+      count++;
+    }
+    if (count !== 0) {
+      var out = storage[0];
+      delete storage[0];
+      return out;
+    } else {
+      return null;
+    }
   };
 
   someInstance.size = function() {
+    var count = 0;
+    for (var key in storage) {
+      count++;
+    }
+    if (count !== 0) {
+      return count;
+    } else {
+      return 0;
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[length] = value;
+    length++;
+    return length;
   };
 
   someInstance.pop = function() {
+    if (length === 0) {
+      return undefined;
+    }
+    var out = storage[length - 1];
+    delete storage[length - 1];
+    length--;
+    return out;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
