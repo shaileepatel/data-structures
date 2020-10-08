9de115d5f7edd3193ecade0cf85962e2c9e134d1
Shailee Patel <shaileeee@gmail.com>
2020-10-08 00:09:04 -0700

    Complete Queue with ES6 instantiation

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,29 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.length = 0;
+    this.first = 0;
+  }
+
+  enqueue(val) {
+    this.storage[this.first + this.length] = val;
+    this.length++;
+    return this.length;
+  }
+
+  dequeue() {
+    if (this.length === 0) {
+      return undefined;
+    }
+    var out = this.storage[this.first];
+    delete this.storage[this.first];
+    this.length--;
+    this.first++;
+    return out;
+  }
+
+  size() {
+    return this.length;
   }
 
 }
