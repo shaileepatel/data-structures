a4f704a1f568c528d5a9d3b371309b1fe159f24d
Shailee Patel <shaileeee@gmail.com>
2020-10-08 11:06:52 -0700

    Complete Stack and Queue with Pseudoclassical instantiation

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.length = 0;
+  this.first = 0;
+};
+
+Queue.prototype.enqueue = function(val) {
+  this.storage[this.first + this.length] = val;
+  this.length++;
+  return this.length;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.length === 0) {
+    return undefined;
+  }
+  var out = this.storage[this.first];
+  delete this.storage[this.first];
+  this.length--;
+  this.first++;
+  return out;
+};
+
+Queue.prototype.size = function() {
+  return this.length;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.length = 0;
+};
+
+Stack.prototype.push = function(val) {
+  this.storage[this.length] = val;
+  this.length++;
+  return this.length;
+};
+
+Stack.prototype.pop = function() {
+  if (this.length === 0) {
+    return undefined;
+  }
+  var out = this.storage[this.length - 1];
+  delete this.storage[this.length - 1];
+  this.length--;
+  return out;
+};
+
+Stack.prototype.size = function () {
+  return this.length;
 };
 
 
