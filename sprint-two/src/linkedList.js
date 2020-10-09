80c1a33ad30910706b52b30deba56bfb41f8776c
Shailee Patel <shaileeee@gmail.com>
2020-10-08 17:22:19 -0700

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,40 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (this.head === null) {
+      return undefined;
+    } else {
+      var removeNode = this.head;
+      this.head = this.head.next;
+      removeNode.next = null;
+      return removeNode.value;
+    }
   };
 
   list.contains = function(target) {
+    if (this.head === null) {
+      return undefined;
+    }
+    var current = this.head;
+    while (current) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   };
-
   return list;
 };
 
@@ -26,4 +52,5 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addToTail and removeHead is O(1) - constant, contains is O(n) - linear.
  */
