ed0a9037386bab9cd52d17ceebeb1a625f8696be
Shailee Patel <shaileeee@gmail.com>
2020-10-10 02:25:27 -0700

    Complete Hash Tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -11,27 +11,51 @@ var HashNode = function(key, value) {
 HashTable.prototype.insert = function(k, v) {
   var newNode = new HashNode(k, v);
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, newNode);
+  var retrieveNodes = this._storage.get(index);
+  if (retrieveNodes === undefined) {
+    this._storage.set(index, [newNode]);
+    return;
+  }
+  for ( var i = 0; i < retrieveNodes.length; i++) {
+    if (retrieveNodes[i].key === k) {
+      retrieveNodes[i].value = v;
+      return;
+    }
+  }
+  retrieveNodes.push(newNode);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var retrieveNode = this._storage.get(index);
-  if (retrieveNode === undefined) {
+  var retrieveNodes = this._storage.get(index);
+  if (retrieveNodes === undefined) {
     return undefined;
   }
-  return retrieveNode.value;
+  for (var i = 0; i < retrieveNodes.length; i++) {
+    if (retrieveNodes[i].key === k) {
+      return retrieveNodes[i].value;
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
+  var retrieveNodes = this._storage.get(index);
+  if (retrieveNodes === undefined) {
+    return undefined;
+  }
+  for (var i = 0; i < retrieveNodes.length; i++) {
+    if (retrieveNodes[i].key === k) {
+      retrieveNodes.splice(i, 1);
+    }
+  }
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert - O(1)
+ * retrieve - O(1)
+ * remove - O(1)
  */
 
 
